13ffa6e3848ad0c7ee3c96a0829028d102950e5b
Rene Casco <rene.casco@icloud.com>
2018-11-04 18:48:06 -0700

    the_offsets method completed

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,15 +6,18 @@ class Shift
   end
 
   def the_keys(keys)
-    characters = keys.chars
-    characters.map.with_index do |character, index|
+    keys.chars.map.with_index do |character, index|
       if index < 4
-        character += characters[index + 1]
-        character.to_i
+        (character += keys[index + 1]).to_i
       end
     end.compact
   end
 
+  def the_offsets(date)
+    date_string = date.strftime('%d') + date.strftime('%m') + date.strftime('%y')
+    offsets = (date_string.to_i ** 2).to_s[-4..-1].chars
+    offsets.map {|offset| offset.to_i}
+  end
 
 
 
