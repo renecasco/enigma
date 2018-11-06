6434cf88345ad2c4c5a119ed095254da591a7b60
Rene Casco <rene.casco@icloud.com>
2018-11-06 13:49:59 -0700

    Initial commit for enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,4 +2,14 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/rotations'
+require './lib/translate'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+
+end
