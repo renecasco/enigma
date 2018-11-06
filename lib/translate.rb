80e89382e4501322f6bdcb43c0857ed2d2b88a31
Rene Casco <rene.casco@icloud.com>
2018-11-06 16:43:11 -0700

    decrypt method completed

--- lib/translate.rb
+++ lib/translate.rb
@@ -1,4 +1,3 @@
-require './lib/shift'
 require 'pry'
 
 class Translate
@@ -6,14 +5,12 @@ class Translate
 
   def initialize
     @character_set = ("a".."z").to_a << " "
-    @shift = Shift.new
   end
 
-  def do_rotations(message, key, date)
-    shifts_array = @shift.final_shift(key, date)
+  def do_rotations(message, keys)
     message.downcase.chars.map.with_index do |char, index|
       if @character_set.include?(char)
-        rotated_character_set = @character_set.rotate(shifts_array[index % 4])
+        rotated_character_set = @character_set.rotate(keys[index % 4])
         char_index = @character_set.find_index(char)
         rotated_character_set[char_index]
       else
