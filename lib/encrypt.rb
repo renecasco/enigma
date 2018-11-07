d2506ac09965eb797ed713a260ffb567def6936c
Rene Casco <rene.casco@icloud.com>
2018-11-07 00:05:30 -0700

    encrypt runner completed, added decrypted.txt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,11 @@
 require './lib/enigma'
 
-  input_file = File.open(ARGV[0], "r")
-  message = input_file.read.chomp
-
   enigma = Enigma.new
-  encrypted = enigma.encrypt(message, "82648")
-
+  input_file = File.open(ARGV[0], "r")
   output_file = File.open(ARGV[1], "w")
-  output_file.write(encrypted[:encryption])
 
+  message = input_file.read.chomp
+  encrypted = enigma.encrypt(message)
+
+  output_file.write(encrypted[:encryption])
   puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
