77dd455596674ad8bee85e8480a2f9bd441f8365
Rene Casco <rene.casco@icloud.com>
2018-11-06 20:25:13 -0700

    last commit before command line work

--- lib/translate.rb
+++ lib/translate.rb
@@ -10,8 +10,8 @@ class Translate
   def do_rotations(message, keys)
     message.downcase.chars.map.with_index do |char, index|
       if @character_set.include?(char)
-        rotated_character_set = @character_set.rotate(keys[index % 4])
         char_index = @character_set.find_index(char)
+        rotated_character_set = @character_set.rotate(keys[index % 4])
         rotated_character_set[char_index]
       else
         char
