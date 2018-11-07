e3c6f07df66b5aa09ccff699a9d1a239c0e1790c
Rene Casco <rene.casco@icloud.com>
2018-11-06 19:12:18 -0700

    helper class and tests completed

--- lib/helper.rb
+++ lib/helper.rb
@@ -4,4 +4,8 @@ class Helper
     Array.new(5){|index|rand(0..9)}.join
   end
 
+  def date_conversion(date)
+    date.strftime('%d%m%y')
+  end
+
 end
--- test/helper_test.rb
+++ test/helper_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/helper'
+require 'Date'
 
 class HelperTest < Minitest::Test
 
@@ -11,13 +12,13 @@ class HelperTest < Minitest::Test
 
   def test_it_generates_5_digit_random_key
     helper = Helper.new
-    assert_equal 5, enigma.random_key.length
+    assert_equal 5, helper.random_key.length
   end
 
   def test_it_coverts_date_format
     helper = Helper.new
-    assert_equal "040895", date_conversion(Date.parse("1995-08-04"))
+    date = Date.parse("1995-08-04")
+    assert_equal "040895", helper.date_conversion(date)
   end
-
 
 end
